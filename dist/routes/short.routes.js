2b17b78363362f68ecaf8b1b5c3699d349f251fc
Robert Meza Garcia <romega_2011@hotmail.com>
2021-01-03 21:32:42 -0500

    first right commit

--- dist/routes/short.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var short_services_1 = require("../services/short.services");
-var router = express_1.Router();
-router.post('/', short_services_1.shortUrl);
-router.get('/:id', short_services_1.getUrl);
-exports.default = router;
